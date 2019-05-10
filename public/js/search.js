6130c5d717ccfd843fb87709b1f9beac9edee92b
travisbare <travisbare@gmail.com>
2019-05-09 19:51:04 -0500

    fixed radio buttons and js for search

---
+++ public/js/search.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#submit").click(function () {
+        var avail = $("input[name='opt2radio']:checked").val();
+        var searchSkill = $("input[name='optradio']:checked").val();
+        if (searchSkill) {
+            alert("Your search term is " + searchSkill);
+        }
+        if (avail){
+            alert("Your availability is "+avail);
+        }
+    });
+
+});
